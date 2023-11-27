13da69ec21290095e0218496122bea093990db22
Aki71 <94998450+Aki71@users.noreply.github.com>
2023-11-27 12:16:27 +0100

    Pocetak

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const sportovi = [
+    "Fudbal","Kosarka","Tenis"
+];
+
+
+document.getElementById('klik').addEventListener('click', function() {
+    // Change the URL to the desired site
+    window.location.href = 'https://unused-css.com/blog/round-buttons/';
+  });
